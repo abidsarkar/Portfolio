7e40048a1427ec7705e100922e218451abaa3b14
abidsarkar <abidsarkar0@gmail.com>
2025-03-08 13:07:49 +0600

    add motion library and add color code in theme in index.css for tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-
-import './App.css'
-import PortfolioSEO from './components/PortfolioSEO '
+import "./App.css";
+import PortfolioSEO from "./components/PortfolioSEO ";
+import { motion } from "motion/react";
 
 function App() {
-  
   return (
     <>
-      <PortfolioSEO/>
-      abcd
+      <PortfolioSEO />
+      <div className="box h-44 w-44 bg-blue-500 m-10 text-xl font-poppins">
+       abcd
+      </div>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
